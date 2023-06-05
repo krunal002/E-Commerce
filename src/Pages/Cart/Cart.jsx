f0720776fa825e2baf024bd44e7fa05ffc0d3010
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-06-06 00:00:21 +0530

    update

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import "./Cart.css";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../E-Commerse";
@@ -9,38 +10,53 @@ const Cart = () => {
     console.log("cart", cartData);
   };
   return (
-    <div>
+    <div className="cart">
       <h2>Cart is ready</h2>
       <button onClick={clickHandle}>click</button>
       <Link to="/" className="headerLink">
         Home
       </Link>
 
-      {cartData.map((product) => {
-        return (
-          <div key={product.id} className="storeProducts">
-            <Link to={`/product-details/${product.id}`}>
-              <img
-                src={product.image}
-                alt="clothingImage"
-                className="storeImage"
-              />
-            </Link>
+      <div className="cartContainer">
+        <div className="leftCartContainer">
+          {cartData.map((product) => {
+            return (
+              <div key={product.id}>
+                <div className="image">
+                    <img
+                      src={product.image}
+                      alt="clothingImage"
+                      className="storeImage"
+                    />
+                </div>
+                <div className="information">
+                  <p>
+                    Price : ₹{product.sellingPrice}{" "}
+                    <span className="printedPrice">{product.price}</span>
+                  </p>
+                  <p>Category : {product.category}</p>
+                  <p>Rating : {product.rating}/5</p>
+                  <button
+                  // onClick={() => wishlisthandler(product)}
+                  >
+                    Add to wishlist
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
 
-            <p>
-              Price : ₹{product.sellingPrice}{" "}
-              <span className="printedPrice">{product.price}</span>
-            </p>
-            <p>Category : {product.category}</p>
-            <p>Rating : {product.rating}/5</p>
-            <button
-            // onClick={() => wishlisthandler(product)}
-            >
-              Add to wishlist
-            </button>
-          </div>
-        );
-      })}
+        <div className="rightCartContainer">
+          <b>Price Details</b>
+          <p>Price : </p>
+          <p>Discount : </p>
+          <p>Delivery Charges : </p>
+          <b>Total Amount : </b>
+          <p>You will save ₹0000 on this order</p>
+          <button>Place Order</button>
+        </div>
+      </div>
     </div>
   );
 };
