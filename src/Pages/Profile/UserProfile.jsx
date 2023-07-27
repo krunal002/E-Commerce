833383b0a1e8ee309219d2aacf49b417af145aaa
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-07-27 16:18:50 +0530

    profile picture modified

--- src/Pages/Profile/UserProfile.jsx
+++ src/Pages/Profile/UserProfile.jsx
@@ -1,19 +1,32 @@
+import "./Profile.css"
 import { useContext } from "react";
 import { LoginContext } from "../../Context/LoginContext";
 import Header from "../../Component/Header/Header";
 
 const UserProfile = () => {
-    const { currUser } = useContext(LoginContext)
-    const { firstName, lastName, email } = currUser;
-    
-    return <div>
-        <Header />
+  const { currUser } = useContext(LoginContext);
+  const { firstName, lastName, email } = currUser;
 
-        <img src="https://www.unicodeconference.org/images/photos/user.jpg" alt="usserImage"/>
-        <p><b>First Name : </b> {firstName}</p>
-        <p><b>Last Name : </b> {lastName}</p>
-        <p><b>Email : </b> {email}</p>
+  return (
+    <div>
+      <Header />
+      <div className="profile-container">
+        <img
+          src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png"
+          alt="usserImage"
+          className="profile-picture"
+        />
+        <p>
+          <b>First Name : </b> {firstName}
+        </p>
+        <p>
+          <b>Last Name : </b> {lastName}
+        </p>
+        <p>
+          <b>Email : </b> {email}
+        </p>
+      </div>
     </div>
-}
+  );
+};
 export default UserProfile;
-
